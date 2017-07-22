9b364b268a879b8e81bedbcd6cdddffdd3ed8e0b
Alexis La Goutte <alexis.lagoutte@gmail.com>
2017-07-22 10:43:34 +0000

    Try to use Qt4

--- tools/debian-setup.sh
+++ tools/debian-setup.sh
@@ -57,7 +57,7 @@ ADDITIONAL_LIST="libnl-3-dev qttools5-dev qttools5-dev-tools libgtk-3-dev \
 		libc-ares-dev libkrb5-dev libqt5svg5-dev lynx libsmi2-dev \
 		portaudio19-dev asciidoc libsbc-dev libgeoip-dev \
 		qtmultimedia5-dev liblua5.2-dev libnl-cli-3-dev \
-		libparse-yapp-perl qt5-default cmake libcap-dev \
+		libparse-yapp-perl qt4-default cmake libcap-dev \
 		liblz4-dev libsnappy-dev libspandsp-dev libxml2-dev"
 
 # Adds package $2 to list variable $1 if the package is found
